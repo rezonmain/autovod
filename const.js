33bd679552593fbd12b15587aedee56bb852a228
rezonmain <adiazgomez101@gmail.com>
2024-08-13 22:22:04 -0700

    feat(twitch-notifier): implement verifyMessage

--- const.js
+++ const.js
@@ -2,3 +2,9 @@ export const TWITCH_TOKEN_URL = "https://id.twitch.tv/oauth2/token";
 export const CACHE_KEYS = {
   TWITCH_ACCESS: "@twitch-access",
 };
+export const TWITCH_WEBHOOK_HEADERS = {
+  MESSAGE_ID: "Twitch-Eventsub-Message-Id".toLowerCase(),
+  MESSAGE_TIMESTAMP: "Twitch-Eventsub-Message-Timestamp".toLowerCase(),
+  MESSAGE_SIGNATURE: "Twitch-Eventsub-Message-Signature".toLowerCase(),
+};
+export const TWITCH_WEBHOOK_HMAC_PREFIX = "sha256=";
--- modules/twitch-notifier.js
+++ modules/twitch-notifier.js
@@ -1,10 +1,38 @@
-/** @import { TwitchAccess } from './twitch-auth' */
+import crypto from "crypto";
+import { TWITCH_WEBHOOK_HEADERS, TWITCH_WEBHOOK_HMAC_PREFIX } from "../const";
+import { empty } from "../utils";
 
-/** @type {TwitchAccess} */
-// const twitchAccess = {
-//   accessToken: "",
-//   expiresIn: 0,
-//   tokenType: "",
-// };
+export const twitchNotifier = {
+  subscribeToWebhook: () => {},
 
-export const twitchNotifier = {};
+  /**
+   * https://dev.twitch.tv/docs/eventsub/handling-webhook-events/#verifying-the-event-message
+   * @param {Response} response
+   * @returns {Promise<[Error, boolean]>}
+   */
+  verifyMessage: (response) => {
+    const secret = process.env.TWITCH_WEBHOOK_SECRET;
+
+    try {
+      if (empty(secret)) {
+        throw new Error("Webhook Secret is required");
+      }
+
+      const message =
+        response.headers[TWITCH_WEBHOOK_HEADERS.MESSAGE_ID] +
+        response.headers[TWITCH_WEBHOOK_HEADERS.MESSAGE_TIMESTAMP] +
+        response.body;
+
+      const hmac =
+        TWITCH_WEBHOOK_HMAC_PREFIX +
+        crypto.createHmac("sha256", secret).update(message).digest("hex");
+
+      return crypto.timingSafeEqual(
+        Buffer.from(hmac),
+        Buffer.from(response.headers[TWITCH_WEBHOOK_HEADERS.MESSAGE_SIGNATURE])
+      );
+    } catch (error) {
+      return [error, false];
+    }
+  },
+};
