a08fd444bb250953120e3bdce64d3dbdeb346f83
rezonmain <adiazgomez101@gmail.com>
2024-08-14 11:51:42 -0700

    feat(server): add port as env

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { callbackTwitchController } from "./controllers/callback-twitch.controller.js";
 import { pingController } from "./controllers/ping.controller.js";
+import { empty } from "./utils.js";
 
 const server = express();
 
 server.use("/callback/twitch", callbackTwitchController);
 server.use("/ping", pingController);
 
-server.listen(6666, () => {
-  console.log("Server started on http://localhost:6666");
+const port = process.env.LISTEN_PORT;
+
+if (empty(port)) {
+  console.error("LISTEN_PORT is not set");
+  process.exit(1);
+}
+
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
