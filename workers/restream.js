90f98710dff84c41315edd65b451a2704ee14cfc
rezonmain <adiazgomez101@gmail.com>
2024-08-16 15:35:31 -0700

    feat(restream): add ffmpeg logic to restream worker

--- workers/restream.js
+++ workers/restream.js
@@ -1,18 +1,54 @@
 import { parentPort } from "node:worker_threads";
+import { ffmpeg } from "../modules/ffmpeg.js";
+import { twitchPlaylist } from "../modules/twitch-playlist.js";
+import { nil } from "../utils.js";
+
+/**
+ * @type {import("child_process").ChildProcess | null}
+ */
+let process = null;
 
 /**
  * @param {string} login
  */
-function start(login) {
+async function start(login) {
   console.log(
     `[${new Date().toISOString()}][RestreamWorker] Starting restream for ${login}`
   );
+  const [accessError, access] = await twitchPlaylist.getPlaybackAccessToken(
+    login
+  );
+
+  if (accessError) {
+    console.error(
+      `[${new Date().toISOString()}][RestreamWorker] Error getting access token`,
+      accessError
+    );
+    return;
+  }
+  const playlistUrl = twitchPlaylist.buildM3u8Url(login, access);
+  process = ffmpeg.restreamToTY(playlistUrl);
 }
 
 function stop() {
+  if (nil(process) || process.killed) {
+    console.log(
+      `[${new Date().toISOString()}][RestreamWorker] Restream ffmpeg process is not running`
+    );
+    process = null;
+    return;
+  }
   console.log(
     `[${new Date().toISOString()}][RestreamWorker] Stopping restream`
   );
+  const killed = process.kill("SIGINT");
+
+  if (!killed) {
+    console.error(
+      `[${new Date().toISOString()}][RestreamWorker] Error killing restream process`
+    );
+  }
+  process = null;
 }
 
 parentPort.on("message", (data) => {
