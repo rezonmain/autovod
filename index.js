0bc3c0775671850a3632ec000d9c4de318245328
rezonmain <adiazgomez101@gmail.com>
2024-08-15 20:22:28 -0700

    feat(app): only serve on start

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// import { server } from "./modules/server.js";
-import { TwitchApi } from "./modules/twitch-api.js";
-import { twitchAuth } from "./modules/twitch-auth.js";
-// import { twitchNotifier } from "./modules/twitch-notifier.js";
+import { server } from "./modules/server.js";
 
-// server.start();
-
-const [tokenError, accessToken] = await twitchAuth.getAccessToken();
-
-if (tokenError) {
-  console.error("Error getting Twitch access token", tokenError);
-  process.exit(1);
-}
-
-const twitchApi = new TwitchApi(accessToken);
-
-const [subsError, subscriptions] = await twitchApi.listFormattedSubscriptions();
-
-console.log(subscriptions);
-
-if (subsError) {
-  console.error("Error getting Twitch subscriptions", subsError);
-  process.exit(1);
-}
+server.start();
