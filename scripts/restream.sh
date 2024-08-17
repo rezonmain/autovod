e8ce9dde4a144af22b6d48b860de48ad75427fea
rezonmain <adiazgomez101@gmail.com>
2024-08-17 02:40:16 -0700

    feat(script): add bash restream script

---
+++ scripts/restream.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ffmpeg -hide_banner \
+      -re \
+      -i "$2" \
+      -map 0:v:0 -map 0:a:0 \
+      -c:v copy \
+      -c:a copy \
+      -f hls \
+            -hls_time 6 \
+            -hls_list_size 4 \
+            -hls_delete_threshold 1 \
+            -hls_start_number_source datetime \
+            "$1";
