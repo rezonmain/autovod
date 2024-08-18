50855fcde3209d787376316d46fdf6f2a35a5b40
rezonmain <adiazgomez101@gmail.com>
2024-08-18 01:56:26 -0700

    chore(app): bring back index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// import { SECRETS } from "./const.js";
-// import { server } from "./modules/server.js";
+import { server } from "./modules/server.js";
 import "./modules/singleton-restream-worker.js";
-import { ytAuth } from "./modules/yt-auth.js";
-// import { readPrivateKey } from "./utils/secrets.js";
 
-// server.start();
-
-const [error, key] = await ytAuth.getAccessToken();
-
-if (error) {
-  console.error(error);
-  process.exit(1);
-}
-
-console.log(key);
+server.start();
