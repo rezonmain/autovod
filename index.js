ea270d59e8aecfdf8f13fd4200bacfd049fe0df9
rezonmain <adiazgomez101@gmail.com>
2024-08-25 14:22:08 -0700

    feat(app): start sever first

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import { server } from "./modules/server.js";
 import { Telegram } from "./modules/telegram.js";
 import { YTStreamManager } from "./modules/youtube-stream-manager.js";
 
+server.start();
 YTStreamManager.getInstance().init();
 Telegram.getInstance().init();
-server.start();
