569882b746d3e7481b0ff664b572caa2d1c8283e
rezonmain <adiazgomez101@gmail.com>
2024-08-27 23:41:06 -0700

    feat(event-log): log app initialization

--- modules/event-log.js
+++ modules/event-log.js
@@ -1,10 +1,22 @@
+import { Database } from "./database.js";
+import { log as appLogger } from "./log.js";
+
 export const eventLog = {
   /**
    * @param {string} message
    * @param {"info" | "error"} type
    * @param {Record<string, any>} metadata
    */
-  log: async (message, type, metadata = {}) => {
-    console.log(metadata);
+  log(message, type, metadata = {}) {
+    const createdAt = new Date().toISOString();
+    const db = Database.getInstance();
+    try {
+      const query = db.prepare(
+        "INSERT INTO events (type, message, metadata, createdAt) VALUES (?, ?, ?, ?)"
+      );
+      query.run(type, message, JSON.stringify(metadata), createdAt);
+    } catch (error) {
+      appLogger.error(`[EventLog] Unable to insert event log ${error.message}`);
+    }
   },
 };
