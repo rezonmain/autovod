1dd09d40cbad37535558cb04335b7c70353fed73
rezonmain <adiazgomez101@gmail.com>
2024-08-31 22:17:48 -0700

    feat(ffmpeg): remove on exit log

--- modules/ffmpeg.js
+++ modules/ffmpeg.js
@@ -2,8 +2,6 @@ import { spawn, exec as syncExec } from "node:child_process";
 import { promisify } from "node:util";
 import path from "node:path";
 import { SCRIPTS } from "../const.js";
-import { RingBuffer } from "./ring-buffer.js";
-import { eventLog } from "./event-log.js";
 
 const exec = promisify(syncExec);
 
@@ -29,29 +27,22 @@ export const ffmpeg = {
    * @returns
    */
   passthroughHLS: ({ sourceUrl, destinationUrl, onExit }) => {
-    const logBuffer = new RingBuffer(20);
-
     const childProcess = spawn(
       path.join(SCRIPTS_PATH, SCRIPTS.PASSTHROUGH_HLS),
       [destinationUrl, sourceUrl]
     );
 
-    childProcess.stdout.on("data", (data) => {
-      logBuffer.push(`[${Date.now().toString()}] ${data.toString()}`);
-    });
+    // childProcess.stdout.on("data", (data) => {
+    //   logBuffer.push(`[${Date.now().toString()}] ${data.toString()}`);
+    // });
 
-    childProcess.stderr.on("data", (data) => {
-      logBuffer.push(`[${Date.now().toString()}] ${data.toString()}`);
-    });
+    // childProcess.stderr.on("data", (data) => {
+    //   logBuffer.push(`[${Date.now().toString()}] ${data.toString()}`);
+    // });
 
     // ffmpeg will exit when the source stream ends
     childProcess.on("exit", async (code) => {
       await onExit(code);
-
-      eventLog.log("[ffmpeg] Exited", "debug", {
-        code,
-        logs: logBuffer.buffer,
-      });
     });
 
     return childProcess;
